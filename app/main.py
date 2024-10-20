1292b0ac1824ee1da4eab4322d28ff75b0af16f4
glaciemdeus <equesimmortal@gmail.com>
2024-10-20 20:27:04 +0300

    Fix readme

--- app/main.py
+++ app/main.py
@@ -24,8 +24,6 @@ app.include_router(tasks_router)
   
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
 ]
